4ae9dd87ec6e4229957184963580306483453ff5
Grifst <kuzia91grifst@gmail.com>
2021-03-12 19:05:36 +1000

    SpawnControl  и условие победы

--- Assets/Scripts/SpawnEnemyControler.cs
+++ Assets/Scripts/SpawnEnemyControler.cs
@@ -7,39 +7,92 @@ public class SpawnEnemyControler : MonoBehaviour
 {
 
     [SerializeField] private GameObject prefEnemy;
-    [SerializeField] private Transform spawnEnemy ;
     [SerializeField] private StatSetter _statSetterEnemy;
     [SerializeField] private int kills;
     [SerializeField] private Text killsUi;
+    [SerializeField] private int tankSpawn;
+    [SerializeField] private int tankRound;
+    [SerializeField] private List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
+    [SerializeField] private List<SpawnPoint> tempSpawnPoint = new List<SpawnPoint>();
+    [SerializeField] private List<Enemy> tempTankOnScene = new List<Enemy>();
 
-  // [SerializeField] private List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
 
     void Start()
     {
-        EnemyGo();
+       
         killsUi.text = kills.ToString();
-       // spawnPoints.AddRange(FindObjectsOfType<SpawnPoint>());
+        spawnPoints.AddRange(FindObjectsOfType<SpawnPoint>());
+        tempSpawnPoint.AddRange(spawnPoints);
+        EnemyGo();
+
     }
 
+   
 
     private void EnemyGo()
     {
-        EnemyCreate(prefEnemy).transform.position = spawnEnemy.transform.position;
+        // Условие победы
+        if (tankRound <= 0)
+        {
+            print("заработало");
+            return;
 
+        }
+
+
+        
+       // var  tankOnScene = FindObjectsOfType<Enemy>();
+        
+
+        if (tempTankOnScene.Count >= tankSpawn) return;
+
+        var tankFactory = tankSpawn - tempTankOnScene.Count;
+
+
+        tankRound -= tankFactory;
+
+
+        if (tankRound < 0)
+        {
+            tankRound = 0;
+        }
+       
+
+        
+       
+
+        for (int i = 0; i < tankFactory; i++)
+        {
+           
+            if (tempSpawnPoint.Count > 0)
+            {
+                int rnd = Random.Range(0, tempSpawnPoint.Count);
+                EnemyCreate(prefEnemy).transform.position = tempSpawnPoint[rnd].transform.position;
+              
+                tempSpawnPoint.RemoveAt(rnd);
+            }
+            else
+            {
+                tempSpawnPoint.AddRange(spawnPoints);
+            }
+        }
+        
     }
     private GameObject EnemyCreate(GameObject pref)
     {
         var temp = Instantiate(pref);
+        tempTankOnScene.Add(temp.GetComponent<Enemy>());
         temp.GetComponent<HelthControl>().Setter = _statSetterEnemy;
         temp.GetComponent<HelthControl>().OnDead += OnEnemyDead;
         return temp;
     }
-    private void OnEnemyDead(GameObject Enemy)
+    private void OnEnemyDead(GameObject enemy)
     {
-        Enemy.GetComponent<HelthControl>().OnDead -= OnEnemyDead;
+        enemy.GetComponent<HelthControl>().OnDead -= OnEnemyDead;
+        tempTankOnScene.Remove(enemy.GetComponent<Enemy>());
         kills++;
         killsUi.text = kills.ToString();
-        Destroy(Enemy);
+        Destroy(enemy);
         EnemyGo();
     }
 }
