b48c6a05ed4bf976b7bb7cafb29d5dda8fda9c13
Tory <vickasvetlova@gmail.com>
2021-05-13 18:18:02 +0300

    add fire distance

--- Assets/Scripts/RotationGunEnemy.cs
+++ Assets/Scripts/RotationGunEnemy.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class RotationGunEnemy : BaseTank
 {
-
     [SerializeField] Shoting _shoting;
     [SerializeField] Transform target;
     [SerializeField] Transform firepoint;
@@ -11,12 +10,13 @@ public class RotationGunEnemy : BaseTank
     public float timer = 0f;
     public float _rateFireoffspeed = 3f;
     private bool acessFire;
+    [SerializeField] private float fireDistance=10f;
 
     void Start()
     {
-        RaycastHit2D hit = Physics2D.Raycast(firepoint.position, -Vector2.up);
-
+        //   RaycastHit2D hit = Physics2D.Raycast(firepoint.position, -Vector2.up);
     }
+
     public void SetTarget(Transform target)
     {
         this.target = target;
@@ -25,17 +25,20 @@ public class RotationGunEnemy : BaseTank
 
     private void Update()
     {
-        Debug.DrawRay(firepoint.position, transform.up, Color.green);
-        Debug.Log(target);
+        // Debug.DrawRay(firepoint.position, transform.up, Color.green);
+        // Debug.Log(target);
 
         if (target == null) return;
         base.RotationOnTarget(target.transform.position, SpeedTorward);
         GetAngleAttack();
-
-        
-
     }
-    
+
+    private float GetDistance()
+    {
+        if (target == null || this.gameObject == null) return Mathf.Infinity;
+        var distace = Vector2.Distance(transform.position, target.position);
+        return distace;
+    }
 
     private void GetAngleAttack()
     {
@@ -45,9 +48,6 @@ public class RotationGunEnemy : BaseTank
         if (target == null) return;
         if (Vector3.Angle(transform.forward, target.position - transform.position) <= angle)
         {
-
-
-
             timer += Time.deltaTime;
             if (timer >= _rateFireoffspeed)
             {
@@ -56,14 +56,10 @@ public class RotationGunEnemy : BaseTank
 
                 if (acessFire)
                 {
-
+                    if(GetDistance()>fireDistance) return;
                     _shoting.Shoot();
-
                 }
             }
-
         }
-
     }
-
 }
